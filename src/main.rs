0e3397e5679e4e228ea21bbd36aa129f8446a0c6
xavierbrasher <xavier.brasher@gmail.com>
2022-02-21 19:32:50 +0800

    added reading and random. Also added the gameloop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::io::Read;
+use rand::prelude::*;
 
-fn read_file() -> String{
+
+fn read_file() -> String {
     let mut f = fs::File::open("words.txt").expect("Unable to open");
-    let mut s = String::new();
+    let mut s: String = String::new();
     f.read_to_string(&mut s).expect("Could not read file");
     return s;
 }
@@ -30,8 +32,45 @@ fn vector_each_line(words_to_be_split:String) -> Vec<String> {
     return words;
 }
 
-fn main() {
-
-    println!("Hello rust");
-    println!("{:?}", vector_each_line(read_file()));
+fn get_a_random_item(vector:Vec<String>) -> String {
+    let mut rng = rand::thread_rng();
+    let mut tmp_vec: Vec<String> = vector;
+    tmp_vec.shuffle(&mut rng);
+    return tmp_vec[0].to_string();
 }
+
+fn input() -> String {
+    let mut input: String = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    return input;
+}
+
+fn game_loop() {
+    println!("Would you like to play? (y or n): ");
+    let option: String = input().to_lowercase();
+    println!("{}", option);
+    if option == String::from("y") {
+        game();
+    } else if option == String::from("n") {
+        println!("Goodbye!");
+    }
+    
+}
+
+
+fn game() {
+    let words: Vec<String> = vector_each_line(read_file());
+    let word: String = get_a_random_item(words);
+    println!("{}", word);
+}
+
+
+fn main() {
+ 
+    println!("Welcome to wordle 😀 built in rust");
+    game_loop();
+    
+    
+
+}
+
